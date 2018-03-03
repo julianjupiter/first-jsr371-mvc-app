21171aae94886180619656c351ac678de30f054c
Julian Jupiter <julianjupiter.io@gmail.com>
2018-03-03 23:05:30 +0800

    Added web path to pom.xml and registered OzarkCoreFeature to RecourceConfig.

--- src/main/java/io/github/julianjupiter/jsr371mvc/config/ApplicationResourceConfig.java
+++ src/main/java/io/github/julianjupiter/jsr371mvc/config/ApplicationResourceConfig.java
@@ -1,6 +1,7 @@
 package io.github.julianjupiter.jsr371mvc.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.mvcspec.ozark.bootstrap.OzarkCoreFeature;
 import org.mvcspec.ozark.jersey.bootstrap.OzarkJerseyFeature;
 
 public class ApplicationResourceConfig extends ResourceConfig {
@@ -8,6 +9,8 @@ public class ApplicationResourceConfig extends ResourceConfig {
     public ApplicationResourceConfig() {
         packages("io.github.julianjupiter.jsr371mvc");
         register(new ApplicationBinder());
+//        register(new OzarkCoreFeature()); // throws exception
+//        register(OzarkCoreFeature.class); // throws exception
         register(OzarkJerseyFeature.class);
     }
 }
